89e0a1e8c5811ac212ad15654f02f1d2c42c228c
Андрей Николаев <nikolaev_ad@ibox.ru>
2022-12-07 16:31:41 +0200

    сохранение 25 задачи

---
+++ domash_4/zadacha_25/Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+int degree(int n1, int n2)
+{
+    int deg = 1;
+    for (int i = 0; i<n2; i++)
+    {
+        deg=deg*n1;
+    }
+    return deg;
+}
+Console.WriteLine("Введите число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите степень числа");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Число в введенной степени равно {degree(num1,num2)}");
