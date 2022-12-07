cb33779792af18c564ec102ba328acb436de7bae
Андрей Николаев <nikolaev_ad@ibox.ru>
2022-12-07 16:45:59 +0200

    сохранение 29 задачи

---
+++ domash_4/zadacha29/Program.cs
@@ -0,0 +1,22 @@
+﻿//Задача 29: Напишите программу, которая задаёт массив из 8 элементов с клавиатуры и выводит массив на экран.
+
+void fillarray(int[] arr)
+{
+    for(int i=0; i<arr.Length;i++) 
+    {
+        Console.WriteLine($"Введите элемент массива под индексом {i}");
+        arr[i] = Convert.ToInt32(Console.ReadLine());
+    }
+}
+
+void printarray(int[] arr)
+{
+    for (int i = 0; i<arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+}
+
+int[] array = new int [8];
+fillarray(array);
+printarray(array);
